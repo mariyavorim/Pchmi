f06c5134cec64517655cb47e3b456de9e416544a
mariyavorim <Ignateva.Masha.MO10@yandex.ru>
2018-12-07 18:30:18 +0500

    Ради авторизации у Миши

---
+++ Otel/Otel/Авторизация.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Otel
+{
+    public partial class Авторизация : Form
+    {      
+        public Авторизация()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (login.Text == "Администратор")
+            {
+                zacelenie zacelenieForm = new zacelenie();
+                zacelenieForm.Show();
+                this.Hide();
+            }
+            if (login.Text == "Техперсонал")
+            {
+                Техперсонал Form = new Техперсонал();
+                Form.Show();
+                this.Hide();
+            }
+           
+        }      
+
+    }
+}
